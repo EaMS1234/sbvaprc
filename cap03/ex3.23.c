72956bbfed0d57e488043a295c5cdc5aac1a49f5
Erick Augusto <52828716+EaMS1234@users.noreply.github.com>
2023-04-13 20:48:03 -0300

    Finalizados os ex de "for"

---
+++ cap03/ex3.23.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    float notaAtual;
+    float media;
+
+    for (int i = 1; i <= 10; i++)
+    {
+        printf("Nota %02d: ", i);
+        scanf("%f", &notaAtual);
+
+        media += notaAtual;
+    }
+
+    // Calcula a média
+    media = media / 10;
+
+    printf("A media aritmetica das dez notas e: %.2f", media);
+
+    return 0;
+}
