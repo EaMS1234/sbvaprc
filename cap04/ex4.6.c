0ba2e02359c1715e753c89c7dd0761a87dc83ae6
Erick Augusto <52828716+EaMS1234@users.noreply.github.com>
2023-04-18 15:31:45 -0300

    add ex4.6~7.c

---
+++ cap04/ex4.6.c
@@ -0,0 +1,63 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    int busca;
+    int qtd = 0;
+
+    int array[5];
+    int posArray[5] = {-1, -1, -1, -1, -1};
+
+    for (int i = 0; i < 5; i++)
+    {
+        printf("array[%d]: ", i);
+        scanf("%d", &array[i]);
+    }
+
+    printf("Buscar por: ");
+    scanf("%d", &busca);
+
+    for (int i = 0; i < 5; i++)
+    {
+        if (array[i] == busca)
+        {
+            posArray[qtd] = i;
+            qtd++;
+        }
+    }
+
+    if (qtd == 0)
+    {
+        printf("O array nao contem o valor %d.\n", busca);
+    }
+    else if (qtd == 1)
+    {
+        printf("O valor %d foi encontrado no indice %d do array.\n", busca, posArray[0]);
+    }
+    else
+    {
+        printf("O valor %d foi encontrado nos indices ", busca);
+
+        for (int i = 0; i < qtd; i++)
+        {
+            if (posArray[i] != -1)
+            {
+                if (qtd - 2 == i)
+                {
+                    printf("%d e ", posArray[i]);
+                }
+                else if (qtd - 1 == i)
+                {
+                    printf("%d do array.\n", posArray[i]);
+                }
+                else
+                {
+                    printf("%d, ", posArray[i]);
+                }
+            }
+        }
+    }
+
+    return 0;
+}
