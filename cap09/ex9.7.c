e613e67e9779f678bd30905f33150b97c46a6999
Erick Augusto <52828716+EaMS1234@users.noreply.github.com>
2023-06-06 15:15:54 -0300

    add cap9.7~8.c

---
+++ cap09/ex9.7.c
@@ -0,0 +1,63 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <ctype.h>
+
+int main()
+{
+    int k = 0;
+
+    char palavra[256];
+    char pares[5][2][256];
+
+    // Lê as palavras
+    for (int i = 0; i < 5; i++)
+    {
+        for (int j = 0; j < 2; j++)
+        {
+            printf("Par %d, palavra %d: ", i + 1, j + 1);
+
+            // Recebe e salva o array de forma correta
+            fgets(palavra, 256, stdin);
+            palavra[strlen(palavra) - 1] = '\0';
+
+            // Salva o array 'palavra' em 'pares[i][j]'
+            k = 0;
+            while (1)
+            {
+                pares[i][j][k] = palavra[k];
+
+                // Detecta o final de um string
+                if (palavra[k] == '\0')
+                {
+                    break;
+                }
+                else
+                {
+                    k++;
+                }
+            }
+        }
+    }
+
+    // Processa e mostra
+    for (int i = 0; i < 5; i++)
+    {
+        printf("%s - %s: ", pares[i][0], pares[i][1]);
+
+        if (strcmp(pares[i][0], pares[i][1]) == 0)
+        {
+            printf("IGUAIS\n");
+        }
+        else if (strcmp(pares[i][0], pares[i][1]) < 0)
+        {
+            printf("ORDEM CRESCENTE\n");
+        }
+        else if (strcmp(pares[i][0], pares[i][1]) > 0)
+        {
+            printf("ORDEM DECRESCENTE\n");
+        }
+    }
+
+    return 0;
+}
---
+++ cap09/ex9.8.c
@@ -0,0 +1,64 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+int main()
+{
+    int n;
+
+    int idxMaior;
+    int idxMeio;
+    int idxMenor;
+
+    char entrada[256];
+    char string[3][256];
+
+    for (int i = 0; i < 3; i++)
+    {
+        printf("String %d: ", i + 1);
+
+        fgets(entrada, 256, stdin);
+        entrada[strlen(entrada) - 1] = '\0';
+
+        n = 0;
+        while (1)
+        {
+            string[i][n] = entrada[n];
+
+            if (entrada[n] == '\0')
+            {
+                break;
+            }
+            else
+            {
+                n++;
+            }
+        }
+    }
+
+    if (strcmp(string[0], string[1]) <= 0 && strcmp(string[0], string[2]) <= 0)
+    {
+        idxMaior = 0;
+
+        idxMeio = strcmp(string[1], string[2]) <= 0 ? 1 : 2;
+        idxMenor = strcmp(string[2], string[1]) <= 0 ? 1 : 2;
+    }
+    else if (strcmp(string[1], string[0]) <= 0 && strcmp(string[1], string[2]) <= 0)
+    {
+        idxMaior = 1;
+
+        idxMeio = strcmp(string[0], string[2]) <= 0 ? 0 : 2;
+        idxMenor = strcmp(string[2], string[0]) <= 0 ? 0 : 2;
+    }
+    else if (strcmp(string[2], string[0]) <= 0 && strcmp(string[2], string[1]) <= 0)
+    {
+        idxMaior = 2;
+
+        idxMeio = strcmp(string[0], string[1]) <= 0 ? 0 : 1;
+        idxMenor = strcmp(string[1], string[0]) <= 0 ? 0 : 1;
+    }
+
+    printf("%s, %s e %s\n", string[idxMaior], string[idxMeio], string[idxMenor]);
+
+    return 0;
+}
